1d80b5571bb52103e617f8ae5acd786017413464
Harshavardhan1617 <harshavardhan1617@gmail.com>
2024-03-19 15:38:58 +0530

    configured mongo connection and created schema

---
+++ models/Campgrounds.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CampgroundSchema = new Schema({
+    title: String,
+    price: String,
+    location: String,
+    description: String
+})
+
+module.exports = mongoose.model('Campgrounds', CampgroundSchema)
+
