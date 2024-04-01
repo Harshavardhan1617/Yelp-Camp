edf44a3d2578e8d52a80b449cf57288db57db0a8
Harshavardhan1617 <harshavardhan1617@gmail.com>
2024-04-01 22:07:03 +0530

    updated styling for edit and new pages

--- seeds/index.js
+++ seeds/index.js
@@ -17,14 +17,18 @@ const seedDb = async() => {
     await Campground.deleteMany({});
     for(let i=0; i<50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const random100 = Math.floor(Math.random() * 100);
+        
         const camp = new Campground({
             location :`${cities[random1000].city}, ${cities[random1000].state}`,
             title : `${sample(descriptors)} ${sample(places)}`,
-            image: "source.unsplash.com/collection/1155333",
-            description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto vero deserunt officiis et corporis nobis quo ad, nemo at facilis laudantium alias aut incidunt exercitationem natus, animi cum dolorem? Sed a saepe expedita, molestiae ipsa dolores incidunt neque reiciendis provident commodi asperiores! Atque velit quidem nobis adipisci, consequuntur mollitia perferendis?",
+            image: "source.unsplash.com/collection/1155333/",
+            price: random100,
+            description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto vero deserunt officiis et corporis nobis quo ad, nemo at facilis laudantium alias aut incidunt exercitationem natus, animi cum dolorem? Sed a saepe expedita, molestiae ipsa dolores incidunt neque reiciendis.",
         })
         await camp.save();
     }
+    console.log("saved")
 }
 
 seedDb()
