83f03ce51e3db51d1fb898e9c496a0ce68656c69
Harsha vardhan <harshavardhan1617@gmail.com>
2024-04-06 22:02:05 +0530

    installed mongo docker image and reconfigured db services

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 image api: "source.unsplash.com/collection/1155333"
+mongo docker image ip: 172.17.0.2
--- seeds/index.js
+++ seeds/index.js
@@ -1,34 +1,36 @@
-const {places, descriptors} = require("./seedHelpers")
-const cities = require("./cities")
+const { places, descriptors } = require("./seedHelpers");
+const cities = require("./cities");
 
 const mongoose = require("mongoose");
 const Campground = require("../models/Campgrounds");
 
-mongoose.connect("mongodb://localhost:27017/yelp-camp");
+mongoose.connect("mongodb://172.17.0.2:27017/yelp-camp");
+// mongoose.connect("mongodb://localhost:27017/yelp-camp");
 const db = mongoose.connection;
-db.on("error", err => logError(err));
-db.once("open", ()=> {
-    console.log("Database Connected!!");
-})
+db.on("error", (err) => logError(err));
+db.once("open", () => {
+  console.log("Database Connected!!");
+});
 
-const sample = arr => arr[Math.floor(Math.random() * arr.length)];
+const sample = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
-const seedDb = async() => {
-    await Campground.deleteMany({});
-    for(let i=0; i<50; i++) {
-        const random1000 = Math.floor(Math.random() * 1000);
-        const random100 = Math.floor(Math.random() * 100);
-        
-        const camp = new Campground({
-            location :`${cities[random1000].city}, ${cities[random1000].state}`,
-            title : `${sample(descriptors)} ${sample(places)}`,
-            image: "source.unsplash.com/collection/1155333/",
-            price: random100,
-            description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto vero deserunt officiis et corporis nobis quo ad, nemo at facilis laudantium alias aut incidunt exercitationem natus, animi cum dolorem? Sed a saepe expedita, molestiae ipsa dolores incidunt neque reiciendis.",
-        })
-        await camp.save();
-    }
-    console.log("saved")
-}
+const seedDb = async () => {
+  await Campground.deleteMany({});
+  for (let i = 0; i < 50; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const random100 = Math.floor(Math.random() * 100);
 
-seedDb()
+    const camp = new Campground({
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+      image: "http://source.unsplash.com/collection/1155333/",
+      price: random100,
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto vero deserunt officiis et corporis nobis quo ad, nemo at facilis laudantium alias aut incidunt exercitationem natus, animi cum dolorem? Sed a saepe expedita, molestiae ipsa dolores incidunt neque reiciendis.",
+    });
+    await camp.save();
+  }
+  console.log("saved");
+};
+
+seedDb();
