c018671e38f8c880ff19dcc1c013d4d83188c0e9
Harsha Vardhan <harsha@dev.bbrouter>
2024-05-21 22:51:01 +0530

    added reviews schema

--- models/Campgrounds.js
+++ models/Campgrounds.js
@@ -1,13 +1,20 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+
 const CampgroundSchema = new Schema({
     title: String,
-    price: Number,
     image: String,
+    price: Number,
+    description: String,
     location: String,
-    description: String
-})
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
+});
 
 module.exports = mongoose.model('Campground', CampgroundSchema)
 
