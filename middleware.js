7e2d1700b9b3620564058698d29e4b233c994e59
Harsha vardhan <harshavardhan1617@gmail.com>
2024-07-21 00:09:13 +0530

    added middleware to check logged in status

---
+++ middleware.js
@@ -0,0 +1,7 @@
+const isLoggedIn = (req, res, next) => {
+  if (!req.isAuthenticated()) {
+    req.flash("error", "You Must be Signed in first");
+    return res.redirect("/login");
+  }
+  res.redirect("/new");
+};
