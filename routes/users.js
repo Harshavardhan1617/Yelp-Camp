bd7578a0bc3acb7cd8d7ab15e12f46e9b7c23c95
Harsha vardhan <harshavardhan1617@gmail.com>
2024-07-20 20:38:51 +0530

    added post route for registration

---
+++ routes/users.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const User = require("../models/user");
+const router = express.Router();
+const localStrategy = require("passport-local");
+const catchAsync = require("../utils/catchAsync");
+
+router.get("/register", (req, res) => {
+  res.render("campgrounds/register");
+});
+
+router.post(
+  "/register",
+  catchAsync(async (req, res, next) => {
+    try {
+      const { email, username, password } = req.body;
+      const user = new User({ email, username });
+      const registeredUser = await User.register(user, password);
+      req.flash("success", "welcome to Yelpcamp");
+      res.redirect("/campgrounds");
+    } catch (e) {
+      req.flash("error", e.message);
+      res.redirect("register");
+    }
+  })
+);
+
+module.exports = router;
