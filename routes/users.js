bb2f98d5c18e5947c82b007ebbc27b74318424e1
Harsha vardhan <harshavardhan1617@gmail.com>
2024-07-22 13:54:32 +0530

    fixed register route to stay logged in after registration

--- routes/users.js
+++ routes/users.js
@@ -16,8 +16,11 @@ router.post(
       const { email, username, password } = req.body;
       const user = new User({ email, username });
       const registeredUser = await User.register(user, password);
-      req.flash("success", "welcome to Yelpcamp");
-      res.redirect("/campgrounds");
+      req.login(registeredUser, (err) => {
+        if (err) return next(err);
+        req.flash("success", "welcome to Yelpcamp");
+        res.redirect("/campgrounds");
+      });
     } catch (e) {
       req.flash("error", e.message);
       res.redirect("register");
